3b8d8d9b3fd2fc289ae65531bd84498273e4e525
Francesco Giulino <francescogiulino14@gmail.com>
2025-08-19 14:33:19 +0200

    Small changes.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] protected LightController lightController;
     [SerializeField] protected SoundEventComponent soundEventComponent;
     //[SerializeField] protected AnimationController animationController;
+    [SerializeField] private Transform target;
+    [SerializeField] private float chaseSpeed=3, patrolSpeed=1;
+    private NavMeshAgent navMeshAgent;
+
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        if (!navMeshAgent) Debug.LogError($"No NavMeshAgent found in enemy {name}");
+    }
+
+    private void Update()
+    {
+        if (navMeshAgent)
+        {
+            navMeshAgent.SetDestination(target.position);
+            navMeshAgent.speed = patrolSpeed;
+        }
+    }
 }
