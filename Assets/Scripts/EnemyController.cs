14a016b95037aa3b49abfbb16e6f791f455bfdf4
Francesco Giulino <francescogiulino14@gmail.com>
2025-08-19 11:24:07 +0200

    small changes.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] protected LightController lightController;
+    [SerializeField] protected SoundEventComponent soundEventComponent;
+    //[SerializeField] protected AnimationController animationController;
+}
